6dae17962d19b880cbad205fca9a73a0de3e4699
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-01-12 12:51:50 +0300

    feat: first type styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,18 @@ const groups = [
     container.querySelectorAll('.six'),
     container.querySelectorAll('.seven'),
     container.querySelectorAll('.eight'),
-    container.querySelectorAll('.nine')
+    container.querySelectorAll('.nine'),
+    container.querySelectorAll('.ten')
 ]
 
 function setHover(evt) {
     const target = evt.target.className.split(' ');
-    Array.from(container.querySelectorAll(`.${target[1]}`)).forEach((item) => item.style.opacity = '0.2');
+    Array.from(container.querySelectorAll(`.${target[2]}`)).forEach((item) => item.style.backgroundColor = 'rgba(244, 255, 42, 0.7)');
 }
 
 function deleteHover(evt) {
     const target = evt.target.className.split(' ');
-    Array.from(container.querySelectorAll(`.${target[1]}`)).forEach((item) => item.style.opacity = '1');
+    Array.from(container.querySelectorAll(`.${target[2]}`)).forEach((item) => item.style.backgroundColor = 'rgba(175, 185, 255, 0.7)');
 }
 groups.forEach((group) => {
     Array.from(group).forEach((item) => item.addEventListener('mouseout', deleteHover));
