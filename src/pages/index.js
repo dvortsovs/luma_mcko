6b1bc73dff490a1d7a071c785d3a1e1b8f85fafd
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-01-12 10:51:37 +0300

    feat: add buttons group logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,17 @@
 import './index.css';
+
+const container = document.querySelector('.content')
+const five = container.querySelectorAll('.five');
+
+function setHover(evt) {
+    const target = evt.target.className.split(' ');
+    Array.from(container.querySelectorAll(`.${target[1]}`)).forEach((item) => item.style.opacity = '0.2');
+}
+
+function deleteHover(evt) {
+    const target = evt.target.className.split(' ');
+    Array.from(container.querySelectorAll(`.${target[1]}`)).forEach((item) => item.style.opacity = '1');
+}
+
+Array.from(five).forEach((item) => item.addEventListener('mouseover', setHover));
+Array.from(five).forEach((item) => item.addEventListener('mouseout', deleteHover));
